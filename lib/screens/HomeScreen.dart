5277a542fa87e858997e846174afed289a99518e
Ali Yazdi <ma.yazdi75@aut.ac.ir>
2019-05-22 22:54:01 +0430

    edit home screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:news_flutter_app/screens/TopicScreen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -5,17 +6,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  int index = 0;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: new Stack(
+          children: <Widget>[
+            new Offstage(
+              offstage: _selectedIndex != 0,
+              child: new TickerMode(
+                enabled: _selectedIndex == 0,
+                child: TopicScreen(),
+              ),
+            ),
+          ],
+        ),
         bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
-            currentIndex: index,
+            currentIndex: _selectedIndex,
             onTap: (int index) {
               setState(() {
-                this.index = index;
+                this._selectedIndex = index;
               });
             },
             items: [
